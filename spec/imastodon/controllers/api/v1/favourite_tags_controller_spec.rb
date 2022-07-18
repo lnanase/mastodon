164903c80fa3423e3221191b143d6217330c0250
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2022-07-18 06:13:43 +0000

    未収載タグ付き投稿についてのFanOutOnWriteServiceのspecにonceを追加
    ついでにrubocop

--- spec/imastodon/controllers/api/v1/favourite_tags_controller_spec.rb
+++ spec/imastodon/controllers/api/v1/favourite_tags_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::FavouriteTagsController, type: :controller do
   let(:user)  { Fabricate(:user, account: Fabricate(:account, username: 'alice')) }
   let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
   let(:tag) { Fabricate(:tag, name: tag_name) }
-  
+
   before do
     allow(controller).to receive(:doorkeeper_token) { token }
   end
@@ -18,21 +18,20 @@ RSpec.describe Api::V1::FavouriteTagsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-  
+
   describe 'POST #create' do
     let(:scopes) { 'write:statuses' }
     let(:tag_name) { 'dummy_tag' }
-    let(:params){
+    let(:params) do
       {
         tag: tag_name,
-        visibility: 'public'
+        visibility: 'public',
       }
-    }
+    end
 
     subject { post :create, params: params }
-    
+
     context 'when the tag is a new favourite tag' do
-      
       it 'returns http success' do
         expect { subject }.to change { user.account.favourite_tags.count }.by(1)
         expect(response).to have_http_status(:success)
@@ -42,16 +41,16 @@ RSpec.describe Api::V1::FavouriteTagsController, type: :controller do
         expect { subject }.to change { user.account.favourite_tags.count }.by(1)
         expect(
           JSON.parse(response.body, symbolize_names: true).except(:id)
-        ).to eq ({ name: tag_name, visibility: 'public' })
-      end        
+        ).to eq({ name: tag_name, visibility: 'public' })
+      end
     end
 
     context 'when the tag has already been favourite.' do
       before do
         Fabricate(:favourite_tag, account: user.account, tag: tag)
       end
-      
-      it 'returns http 409' do 
+
+      it 'returns http 409' do
         expect { subject }.not_to change { user.account.favourite_tags.count }
         expect(response).to have_http_status(:conflict)
       end
@@ -60,7 +59,7 @@ RSpec.describe Api::V1::FavouriteTagsController, type: :controller do
         expect { subject }.not_to change { user.account.favourite_tags.count }
         expect(
           JSON.parse(response.body, symbolize_names: true).except(:id)
-        ).to eq ({ name: tag_name, visibility: 'public' })
+        ).to eq({ name: tag_name, visibility: 'public' })
       end
     end
   end
@@ -68,41 +67,41 @@ RSpec.describe Api::V1::FavouriteTagsController, type: :controller do
   describe 'DELETE #destroy' do
     let(:scopes) { 'write:statuses' }
     let(:params) { { tag: tag_name } }
-    
+
     subject { delete :destroy, params: params }
 
     context 'when try to destroy the favourite tag' do
       let(:tag_name) { 'dummy_tag' }
-      
+
       before do
         Fabricate(:favourite_tag, account: user.account, tag: tag)
       end
-  
+
       it 'returns http success' do
-        expect{ subject }.to change { user.account.favourite_tags.count }.by(-1)
+        expect { subject }.to change { user.account.favourite_tags.count }.by(-1)
         expect(response).to have_http_status(:success)
       end
 
       it 'responce has success message by json' do
-        expect{ subject }.to change { user.account.favourite_tags.count }.by(-1)
+        expect { subject }.to change { user.account.favourite_tags.count }.by(-1)
         expect(
           JSON.parse(response.body, symbolize_names: true)
-        ).to eq({"succeeded": true})
+        ).to eq({ "succeeded": true })
       end
     end
-    
+
     context 'when try to destroy an unregistered tag' do
       let(:tag_name) { 'unregistered' }
       it 'returns http 404' do
-        expect{ subject }.not_to change { user.account.favourite_tags.count }
+        expect { subject }.not_to change { user.account.favourite_tags.count }
         expect(response).to have_http_status(:not_found)
       end
 
       it 'responce has fail message by json' do
-        expect{ subject }.not_to change { user.account.favourite_tags.count }
+        expect { subject }.not_to change { user.account.favourite_tags.count }
         expect(
           JSON.parse(response.body, symbolize_names: true)
-        ).to eq({"succeeded": false})
+        ).to eq({ "succeeded": false })
       end
     end
   end
